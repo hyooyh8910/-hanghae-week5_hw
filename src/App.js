e858fafb5523e0d2672a05ae5003ca19cb88f17d
hyooyh8910 <hyooyh8910@naver.com>
2022-06-03 20:48:36 +0900

    app에 홈페이지 view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { Navbar, Container, Nav} from "react-bootstrap";
 
+//page&img
+import "./App.css";
+import LogoImg from "./assets/leafs.jpg";
+
+
+
+//react-bootstrap 에서 가져온 Navbar
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <br />
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">HOME</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#내정보">내정보</Nav.Link>
+            <Nav.Link href="#알림">알림</Nav.Link>
+            <Nav.Link href="#회원가입">회원가입</Nav.Link>
+            <Nav.Link href="#로그인">로그인</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <MainPic/>
+    </>
+   
+    
   );
 }
+
+
+
+
+//styled-components
+const MainPic = styled.div`
+  height: 850px;
+  background-image: url(${LogoImg});
+  background-size: cover;
+  background-position: center;
+`;
+/*
+styled-components 에 img넣는 법 
+
+*/
+
+
+
+
 
 export default App;
